eea5ba2cdd8e056f0e7d1971e980a70e2ff0e8d8
alpha132luka <artilakva.luka140912@gmail.com>
2025-12-10 19:04:15 +0400

    second commit

---
+++ day 43/cw43.js
@@ -0,0 +1,3 @@
+const heading = document.getElementById('myHeading');
+
+heading.textContent = 'ახალი ტექსტი';
---
+++ day 43/hw43.js
@@ -0,0 +1,13 @@
+let name = "Luka";
+let age = 25;
+let isStudent = true;
+let score = 98.5;
+
+function showVariables() {
+    console.log("Name:", name);
+    console.log("Age:", age);
+    console.log("Is Student:", isStudent);
+    console.log("Score:", score);
+
+    console.log("Concatenated:", name + " " + age + " " + isStudent + " " + score);
+}
