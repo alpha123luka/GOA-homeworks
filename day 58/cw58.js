71ed32e99bf8a71908275b0ca474615888f1c70f
alpha132luka <artilakva.luka140912@gmail.com>
2026-02-09 17:47:42 +0400

    second commit

---
+++ day 58/cw58.js
@@ -0,0 +1,40 @@
+//length – აბრუნებს მასივის ან სტრინგის ელემენტების რაოდენობას.
+//popped – ამოღებს ბოლოსდასამატებულ ელემენტს მასივიდან და აბრუნებს მას.
+//joined – აერთიანებს მასივის ელემენტებს ერთ სტრინგში, დელიმიტერის გამოყენებით.
+//at – აბრუნებს მასივის ან სტრინგის ელემენტს მითითებულ ინდექსზე.
+
+
+
+// const Colors = ['red', 'green', 'blue'];
+
+// const Joined = Colors.join('-');
+// console.log(Joined); 
+
+// console.log(Colors.at(1)); 
+
+// console.log(Colors.toString()); 
+
+// const Popped = Colors.pop() 
+// console.log(Popped)
+
+// const connected = Colors.concat(Groups) 
+// console.log(connected)
+
+
+
+// Math.floor – ამრგვალებს რიცხვს ქვედა მთელ რიცხვამდე
+// Math.ceil – ამრგვალებს რიცხვს ზედა მთელ რიცხვამდე
+// Math.round – ამრგვალებს რიცხვს მათემატიკურად, ყველაზე ახლო მთელ რიცხვამდე
+// Math.sign – აბრუნებს რიცხვის ნიშანს (დადებითი, უარყოფითი, ნული)
+// Math.max – აბრუნებს მაქსიმალურ რიცხვს
+// Math.min – აბრუნებს მინიმალურ რიცხვს
+
+const Num = 3.14;
+
+console.log(Math.floor(Num)); 
+console.log(Math.ceil(Num));  
+console.log(Math.round(Num)); 
+console.log(Math.sign(Num));  
+
+console.log(Math.max(1, 2, 3, 4, 5)); 
+console.log(Math.min(1, 2, 3, 4, 5)); 
