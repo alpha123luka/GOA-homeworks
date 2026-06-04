3a5b030bd278d2820df101612a53ae498b068722
alpha132luka <artilakva.luka140912@gmail.com>
2026-06-04 19:02:10 +0400

    second commit

---
+++ day 89/cw89.js
@@ -0,0 +1,7 @@
+//setTimeout() არის ფუნქცია, რომელიც საშუალებას გვაძლევს, რომ კოდი შესრულდეს მითითებული დროის გასვლის შემდეგ.
+
+function logConsole(){
+    console.log('ITS BEEN 10 SECONDS!');
+}
+
+setTimeout(logConsole, 10000)
